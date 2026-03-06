2a2771b43afe6ed06456aaf288c6b4d259df89ad
estefani31-hub <gq1051032024@unab.edu.sv>
2026-03-05 22:33:08 -0600

    Refactor vehicle namespace and implement vehicle list

--- Program.cs
+++ Program.cs
@@ -1,69 +1,49 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Parcial1Programacion1G2
+namespace ParcialProgramacion1G2
 {
-    public class Vehiculo
-    {
-        public string Marca { get; set; }
-        public string Modelo { get; set; }
-        public int Anio { get; set; }
-
-        public Vehiculo(string marca, string modelo, int anio)
-        {
-            Marca = marca;
-            Modelo = modelo;
-            Anio = anio;
-        }
-
-        public virtual void MostrarInformacion()
-        {
-            Console.WriteLine("Vehículo:");
-            Console.WriteLine("Marca: " + Marca);
-            Console.WriteLine("Modelo: " + Modelo);
-            Console.WriteLine("Año: " + Anio);
-        }
-
-        public virtual void Encender()
-        {
-            Console.WriteLine("El vehículo se está encendiendo...");
-        }
-    }
-
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("====================================");
-            Console.WriteLine("   SISTEMA DE VEHICULOS - PARCIAL   ");
-            Console.WriteLine("====================================");
-            Console.WriteLine();
 
             // TODO: Crear una lista de tipo Vehiculo
             // Ejemplo: List<Vehiculo> vehiculos = new List<Vehiculo>();
+
+            List<Vehiculo> vehiculos = new List<Vehiculo>();
 
 
             // TODO: Crear al menos DOS objetos de clases hijas
             // Ejemplo: Carro, Moto
 
+            Carro carro1 = new Carro("Toyota", "Corolla", 2020);
+            Moto moto1 = new Moto("Yamaha", "R3", 2021);
+
 
             // TODO: Agregar los objetos a la lista
+
+            vehiculos.Add(carro1);
+            vehiculos.Add(moto1);
 
 
             // TODO: Recorrer la lista usando foreach
 
+            foreach (Vehiculo v in vehiculos)
+            {
 
-            // TODO: Llamar a los métodos
-            // MostrarInformacion()
-            // Encender()
+                // TODO: Llamar a los métodos
+                // MostrarInformacion()
+                // Encender()
 
+                v.MostrarInformacion();
+                v.Encender();
+
+                Console.WriteLine();
+            }
 
             Console.WriteLine();
             Console.WriteLine("Programa finalizado.");
-            Console.ReadKey();
         }
     }
 }
